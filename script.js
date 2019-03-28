3767d9dd4c3534d593135d5b84b821d6eb5df538
deartist22 <deartist22@gmail.com>
2019-03-28 07:17:33 -0500

    Create script.js
    Add script.js basic structure and is working

---
+++ script.js
@@ -0,0 +1,13 @@
+const container = document.querySelector(".gameBoard");
+const scoreBoard = document.querySelector(".score");
+const btnStart = document.querySelector(".btnStart");
+
+let lasthideout = false;
+let gameOver = false;
+let score = 0;
+
+btnStart.addEventListener('click', startGame);
+
+function startGame(){
+
+}
