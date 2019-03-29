b6c78400496e247b7c60a083387e0536250ef261
deartist22 <deartist22@gmail.com>
2019-03-29 11:44:49 -0500

    Function makeGameBoard
    makeGameBoard function created, tested, working!

--- script.js
+++ script.js
@@ -9,5 +9,21 @@ let score = 0;
 btnStart.addEventListener('click', startGame);
 
 function startGame(){
+	//make board
+	makeGameBoard();
+	//make bad guy
+	scoreBoard.innerHTML = score;
 
+}
+
+function makeGameBoard() {
+	let hidOutsCreated = 8;
+	container.innerHTML = "";
+	for(let x = 0; x < hidOutsCreated; x++){
+		console.log('making');
+		let div = document.createElement("div");
+		div.setAttribute("class", "hideout");
+		div.badGuyId = x;
+		container.appendChild(div);
+	}
 }
